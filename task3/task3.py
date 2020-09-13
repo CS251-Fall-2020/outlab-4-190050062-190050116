1838d9ef46a66d35761511cd6b402f5097de238f
Soham Mistri <smistri@cse.iitb.ac.in>
2020-09-13 12:06:28 +0000

    Final task 3 added

--- task3/task3.py
+++ task3/task3.py
@@ -6,17 +6,18 @@ import argparse
 ap = argparse.ArgumentParser()
 ap.add_argument("--input", required=True)
 ap.add_argument("--k", required=True)
-ap.add_argument("--output", required=False)
+ap.add_argument("--output", required=True)
 args = vars(ap.parse_args())
 
-img=np.array(Image.open(str(args['input'])), np.float)
-M=img.reshape((img.shape[0]*img.shape[1], img.shape[2]))
+img = np.array(Image.open(r"{}".format(str(args['input']))), np.float)
+M = img.reshape((img.shape[0] * img.shape[1], img.shape[2]))
 
-centroid, label=scipy.cluster.vq.kmeans2(M, int(args['k']), iter=100, minit='++', check_finite=True)
-m=M.shape[0]
+centroid, label = scipy.cluster.vq.kmeans2(
+    M, int(args['k']), iter=100, minit='++', check_finite=True)
+m = M.shape[0]
 for i in range(m):
-	M[i, :]=centroid[label[i], :]
+    M[i, :] = centroid[label[i], :]
 
-M=M.reshape((img.shape[0], img.shape[1], img.shape[2]))
+M = M.reshape((img.shape[0], img.shape[1], img.shape[2]))
 new_image = Image.fromarray(M.astype('uint8'), 'RGB')
-new_image.save(str(args['output']))
+new_image.save(r"{}".format(str(args['output'])))
